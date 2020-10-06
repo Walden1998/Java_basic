882b32a2576a59d979be2d1d1067993bbb1a440b
wentao1998 <47709595+wentao1998@users.noreply.github.com>
2020-10-06 16:07:17 +0800

    Add files via upload

---
+++ 1/src/StudentInfo.java
@@ -0,0 +1,12 @@
+/**
+ * @author Walden
+ * @create 2020-09-30 18:29
+ */
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("学号：1111111");
+        System.out.println("姓名：Walden");
+        System.out.println("生日：111111");
+        System.out.println("wonderful");
+    }
+}
