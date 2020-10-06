06947e54747bbae025c5be0dc375019a7258f487
wentao1998 <47709595+wentao1998@users.noreply.github.com>
2020-10-06 16:10:24 +0800

    Add files via upload

---
+++ 3/src/VarPractice.java
@@ -0,0 +1,17 @@
+/**
+ * @author Walden
+ * @create 2020-09-30 18:35
+ */
+public class VarPractice {
+    public static void main(String[] args) {
+        int stuNO1 = 110;
+        String stuName1 = "张三";
+        char stuSex1 = '男';
+        double stuHeight1 = 1.85;
+        System.out.println("stuNO1 = " + stuNO1);
+        System.out.println("stuName1 = " + stuName1);
+        System.out.println("stuSex1 = " + stuSex1);
+        System.out.println("stuHeight1 = " + stuHeight1);
+
+    }
+}
